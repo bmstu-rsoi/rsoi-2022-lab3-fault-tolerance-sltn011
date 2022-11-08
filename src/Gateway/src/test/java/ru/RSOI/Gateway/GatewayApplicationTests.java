de1f12544c9c11b9657f8d2643836b61c1d1097d
Slava Tenyaev <61147580+sltn011@users.noreply.github.com>
2022-11-08 15:45:06 +0300

    lab2 src files

---
+++ src/Gateway/src/test/java/ru/RSOI/Gateway/GatewayApplicationTests.java
@@ -0,0 +1,9 @@
+package ru.RSOI.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+}
---
+++ src/Payment/src/main/java/ru/Error/EError400.java
@@ -0,0 +1,24 @@
+package ru.RSOI.Payment.Error;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EError400 {
+    public String message;
+    public Map<String, String> errors = new HashMap<String, String>();
+
+    EError400(EBadRequestError error)
+    {
+        this.message = error.message;
+        for (int i = 0; i < error.errors.size(); i++)
+        {
+            addError(error.errors.get(i));
+        }
+    }
+
+    public void addError(String error)
+    {
+        String key = new String("additionalProp") + String.format("%d", errors.size() + 1);
+        errors.put(key, error);
+    }
+}
